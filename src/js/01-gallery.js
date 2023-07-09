fd481a154e773038b4eb81dc662724b5d317daf8
Alex <alexlipskyi@icloud.com>
2023-07-09 17:51:48 +0300

    inal

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";   
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (element) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href=${element.original}>
+        <img
+          class="gallery__image"
+          src=${element.preview}
+          alt=${element.description}
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
+
+console.log(galleryItems);
---
+++ src/js/03-feedback.js
@@ -0,0 +1,62 @@
+const throttle = require('lodash.throttle');
+// import { throttle } from "lodash";
+
+const form = document.querySelector('.feedback-form');
+const formEmail = document.querySelector('.feedback-form input');
+const formMessage = document.querySelector('.feedback-form textarea');
+// const button = document.querySelector('button');
+
+const dataStorage = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+isEmpty();
+
+// Вызов слушателей
+formEmail.addEventListener('input', throttle(dataEntry, 500));
+formMessage.addEventListener('input', throttle(dataEntry, 500));
+form.addEventListener('submit', handleSubmit);
+
+// Проверка на наличие данных в localeStoarge
+function isEmpty() {
+ 
+
+  if (dataStorage.email) {
+    formEmail.value = dataStorage.email;
+  }
+
+  if (dataStorage.message) {
+    formMessage.value = dataStorage.message;
+  }
+}
+
+// Сохранение в localeStorage
+
+function dataEntry() {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: formEmail.value,
+      message: formMessage.value,
+    })
+  );
+
+}
+
+// Submit
+
+function handleSubmit(event) {
+
+  event.preventDefault();
+
+  if (!formEmail.value) {
+    alert('Email is required input field');
+    return;
+  }
+
+  if (!formMessage.value) {
+    alert("Message filed can't be empty. Enter at least 1 character!");
+    return;
+  }
+  form.reset();
+    
+  console.log(dataStorage);
+}
