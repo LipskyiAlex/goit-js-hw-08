f78840ee95663a9e219b96239cb31b4e70241147
Alex <alexlipskyi@icloud.com>
2023-07-10 19:23:40 +0300

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -27,3 +27,4 @@ new SimpleLightbox(".gallery a", {
 });
 
 
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,36 @@
+
 const throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
 const formEmail = document.querySelector('.feedback-form input');
 const formMessage = document.querySelector('.feedback-form textarea');
-
 const dataStorage = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-isEmpty();
+
 
 // Вызов слушателей
 formEmail.addEventListener('input', throttle(dataEntry, 500));
 formMessage.addEventListener('input', throttle(dataEntry, 500));
 form.addEventListener('submit', handleSubmit);
 
+isEmpty();
+
 // Проверка на наличие данных в localeStoarge
 function isEmpty() {
- 
+
   if (dataStorage.email) {
     formEmail.value = dataStorage.email;
+  } else {
+
+    formEmail.email = "";
   }
 
   if (dataStorage.message) {
     formMessage.value = dataStorage.message;
+  } else {
+    formMessage.value = ""; 
   }
+
 }
 
 // Сохранение в localeStorage
@@ -44,16 +52,20 @@ function handleSubmit(event) {
 
   event.preventDefault();
 
-  // if (!formEmail.value) {
-  //   alert('Email is required input field');
-  //   return;
-  // }
+  if (!formEmail.value) {
+    alert('Email is required input field');
+    return;
+  }
 
-  // if (!formMessage.value) {
-  //   alert("Message filed can't be empty. Enter at least 1 character!");
-  //   return;
-  // }
-  form.reset();
+  if (!formMessage.value) {
+    alert("Message filed can't be empty. Enter at least 1 character!");
+    return;
+  }
+ 
     
   console.log(dataStorage);
+  form.reset();
 }
+
+
+
